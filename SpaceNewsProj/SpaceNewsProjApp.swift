e2970156a00f96991f42e40dca33d326e54ad1d3
user <A00279780@mycambrian.ca>
2023-11-12 13:00:52 -0500

    Initial Commit

---
+++ SpaceNewsProj/SpaceNewsProjApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceNewsProjApp.swift
+//  SpaceNewsProj
+//
+//  Created by mahek on 12/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct SpaceNewsProjApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
