4ed08673309baa94083bd448a20677a1ae2f3b44
Mahek Tailor <Mahektailor28@gmail.com>
2023-11-27 16:07:58 -0500

    Added core data model to integrate usersname and password storage

--- SpaceNewsProj/Controller/PersistenceController.swift
+++ SpaceNewsProj/Controller/PersistenceController.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+import CoreData
+
+class PersistenceController {
+    static let shared = PersistenceController()
+    
+    let container: NSPersistentContainer
+    
+    init() {
+        container = NSPersistentContainer(name: "SpaceNewsUserModel")
+        container.loadPersistentStores { _, error in
+            if let error = error {
+                fatalError("Error: \(error.localizedDescription)")
+            }
+        }
+    }
+}
+
